50a6a967378f718ee91aa062769ff260cc9e3e3f
Ome Tiwari <omt81144@gmail.com>
2026-04-28 00:36:34 +0530

    Time: 32 ms (92.69%), Space: 45.4 MB (55.1%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,29 +1,20 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        int n = nums.size();
-        vector<int> prefixSum(n);
+        unordered_map<int, int> hash;
+        hash[0] = 1;
 
-        // store prefix sum of them in vecotr
-        prefixSum[0] = nums[0];
-        for(int i=1; i<n; i++){
-            prefixSum[i] = prefixSum[i-1] + nums[i];
-        }
-        
-        int count = 0; // no. of subarrays
-        unordered_map<int, int> hash; // prefixSum, frequency
-        for(int j=0; j<n; j++){
-            if(prefixSum[j] == k) count++;
+        int sum = 0, count = 0;
 
-            int val = prefixSum[j] - k;
-            if(hash.find(val) != hash.end()){
-                count += hash[val];
-            }
+        for(int it : nums){
+            sum += it;
 
-            if(hash.find(prefixSum[j]) == hash.end()){
-                hash[prefixSum[j]] = 0;
-            }
-            hash[prefixSum[j]]++;
+            int removed = sum - k;
+            if(hash.find(removed) != hash.end()){
+                count += hash[removed];
+            } 
+
+            hash[sum]++;
         }
         return count;
     }
