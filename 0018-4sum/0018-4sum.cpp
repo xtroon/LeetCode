db110ada56fcbc0448df31db8565c663ba2685c8
Ome Tiwari <omt81144@gmail.com>
2026-04-29 19:30:47 +0530

    Time: 17 ms (84.88%), Space: 17.3 MB (82.41%) - LeetHub

--- 0018-4sum/0018-4sum.cpp
+++ 0018-4sum/0018-4sum.cpp
@@ -2,27 +2,38 @@ class Solution {
 public:
     vector<vector<int>> fourSum(vector<int>& nums, int target) {
         int n = nums.size();
-        set<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans;
 
         for(int i=0; i<n; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+
             for(int j=i+1; j<n; j++){
-                unordered_set<int> seen;
+                if(j > i+1 && nums[j] == nums[j-1]) continue;
+                int k = j+1, l = n-1;
 
-                for(int k=j+1; k<n; k++){
-                    long long req = (long long)target - nums[i] - nums[j] - nums[k];
+                while(k < l){
+                    long long sum = (long long)nums[i] + nums[j] + nums[k] + nums[l];
 
-                    if(req > INT_MAX || req < INT_MIN) continue;
-                    if(seen.find(req) != seen.end()){
-                        vector<int> vec = { nums[i], nums[j], nums[k], (int)req};
-                        sort(vec.begin(), vec.end());
-                        ans.insert(vec);
+                    if(sum == target){
+
+                        ans.push_back({nums[i], nums[j], nums[k], nums[l]});
+                        while( k<l && nums[k]== nums[k+1]) k++;
+                        while( k<l && nums[l] == nums[l-1]) l--;
+
+                        k++;
+                        l--;
                     }
-
-                    seen.insert(nums[k]);
+                    else if( sum < target){
+                        k++;
+                    }
+                    else{
+                        l--;
+                    }
                 }
+
             }
         }
-
-        return vector<vector<int>>(ans.begin(), ans.end());
+        return ans;
     }
 };
