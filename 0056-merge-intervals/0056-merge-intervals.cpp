1d7b3a37578a920ba7dd5be591408bcce039ac63
Ome Tiwari <omt81144@gmail.com>
2026-04-30 00:49:31 +0530

    Time: 4 ms (66.93%), Space: 23.9 MB (58.92%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -7,23 +7,11 @@ public:
         vector<vector<int>> ans;
 
         for(int i=0; i<n; i++){
-            int st = arr[i][0];
-            int en = arr[i][1];
-
-            if(!ans.empty() && en <= ans.back()[1]){
-                continue;
+            if(ans.empty() || arr[i][0] > ans.back()[1]){
+                ans.push_back(arr[i]);
+            }else{
+                ans.back()[1] = max(ans.back()[1], arr[i][1]);
             }
-
-            for(int j = i+1; j<n; j++){
-                if(arr[j][0] <= en){
-                    en = max(en, arr[j][1]);
-                }else{
-                    break;
-                }
-            }
-
-            ans.push_back({st, en});
-
         }
 
         return ans;
