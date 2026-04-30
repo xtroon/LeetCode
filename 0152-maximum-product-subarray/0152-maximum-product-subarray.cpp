e37a04d38921ec0f236b41ccd53206ef3152c054
Ome Tiwari <omt81144@gmail.com>
2026-05-01 00:01:32 +0530

    Time: 0 ms (100%), Space: 17.8 MB (22.25%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -3,13 +3,20 @@ public:
     int maxProduct(vector<int>& nums) {
         int ans = nums[0];
         int n =nums.size();
-        for(int i=0; i<n; i++){
-            int prod = 1;
-            for(int j=i; j<n; j++){
-                prod *= nums[j];
-                ans = max(ans, prod);
 
+        int prefix = 1;
+        int suffix = 1;
+
+        for(int i=0; i<n; i++){
+            if(prefix == 0){
+                prefix = 1;
             }
+            if(suffix == 0){
+                suffix = 1;
+            }
+            prefix *= nums[i];
+            suffix *= nums[n-i-1];
+            ans = max(ans, max(prefix, suffix));
         }
         return ans;
     }
