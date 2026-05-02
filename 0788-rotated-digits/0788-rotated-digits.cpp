30d45af048cc92798eec7a6bf05e349436b3d2e9
Ome Tiwari <omt81144@gmail.com>
2026-05-03 01:07:35 +0530

    Time: 0 ms (100%), Space: 9.6 MB (22.4%) - LeetHub

--- 0788-rotated-digits/0788-rotated-digits.cpp
+++ 0788-rotated-digits/0788-rotated-digits.cpp
@@ -1,25 +1,29 @@
 class Solution {
 public:
     int rotatedDigits(int n) {
+        vector<int> dp(n+1, 0);
         int cnt = 0;
-        for(int i=1; i<=n; i++){
-            int num = i;
-            bool valid = true;
-            bool same = false;
-            while(num){
-                int rem = num%10;
 
-                if(rem == 7 || rem == 4 || rem == 3){
-                    valid = false;
-                    break;
-                }
-                if(rem == 2 || rem == 6 || rem == 5 || rem==9){
-                    same = true;
-                }
+        // 0 -> invalid
+        // 1 -> ok but not changed
+        //2 -> changed
 
-                num /= 10;
+        for(int i=0; i<=n; i++){
+            if( i < 10){
+                if(i==0 || i==1 || i==8) dp[i] =1;
+                else if(i==3 || i==4 || i==7) dp[i] = 0;
+                else dp[i] = 2;
             }
-            if(valid && same) cnt++;
+            else{
+                int a = dp[i/10];
+                int b = dp[i%10];
+
+                if( a==1 && b==1) dp[i] = 1;
+                else if(a==0 || b==0) dp[i] = 0;
+                else dp[i] = 2;
+            }
+
+            if(dp[i] == 2) cnt++;
         }
         return cnt;
     }
