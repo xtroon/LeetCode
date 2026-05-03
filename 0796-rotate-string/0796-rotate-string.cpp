da978ffccf872abf7d1788a04b4b20a3f850ea94
Ome Tiwari <omt81144@gmail.com>
2026-05-03 09:49:07 +0530

    Time: 0 ms (100%), Space: 8.2 MB (61.66%) - LeetHub

--- 0796-rotate-string/0796-rotate-string.cpp
+++ 0796-rotate-string/0796-rotate-string.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-        if(s.length() != goal.length()) return false;
-        string doubleS = s + s;
-        return doubleS.find(goal) != string::npos;
+        if( s.length() != goal.length()) return false;
+
+        s = s+s;
+        if(s.find(goal) != string::npos) return true;
+
+        return false;
     }
 };
