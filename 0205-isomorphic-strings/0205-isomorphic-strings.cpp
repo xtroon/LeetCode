e465754118d5c765933451b445d5581f0c88d1c9
Ome Tiwari <omt81144@gmail.com>
2026-05-14 15:24:50 +0530

    Time: 0 ms (100%), Space: 9.2 MB (80.6%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.cpp
+++ 0205-isomorphic-strings/0205-isomorphic-strings.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
-        if(s.size()!=t.size()) return false;
-        vector<int> v(256,1000);
-        //checking t with perspective of s
-        for(int i=0; i<s.size();i++){
-            int idx = (int)s[i];
-            if(v[idx]==1000) v[idx] = s[i] - t[i];
-            else if(v[idx]!=(s[i]-t[i])) return false;
-        }
-        //checking t with perspective of s
-        for(int i=0; i<v.size();i++) v[i] = 1000; //reset array
+        if(s.size() != t.size()) return false;
 
-        for(int i=0; i<t.size();i++){
-            int idx = (int)t[i];
-            if(v[idx]==1000) v[idx] = t[i] - s[i];
-            else if(v[idx]!=(t[i]-s[i])) return false;
-        }
+        vector<int> m1(256, 0);
+        vector<int> m2(256, 0);
 
+        for(int i=0; i< s.size(); i++){
+            if(m1[s[i]] != m2[t[i]]) return false;
+
+            m1[s[i]] = i+1;
+            m2[t[i]] = i+1;
+        }
         return true;
     }
 };
