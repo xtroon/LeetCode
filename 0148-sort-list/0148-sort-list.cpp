20b44817a14fc0e59cb26a440260d88832d96290
Ome Tiwari <omt81144@gmail.com>
2026-05-18 12:57:27 +0530

    Time: 55 ms (14.68%), Space: 75.8 MB (6.45%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,26 +10,49 @@
  */
 class Solution {
 public:
+    ListNode* merge(ListNode* list1, ListNode* list2){
+        ListNode* dummy = new ListNode(-1);
+        ListNode* temp = dummy;
+
+        while(list1 != nullptr && list2 != nullptr){
+            if(list1->val >= list2->val){
+                temp->next = list2;
+                list2 = list2->next;
+            }else{
+                temp->next = list1;
+                list1 = list1->next;
+            }
+            temp = temp->next;
+        }
+
+        if(list1 != nullptr){
+            temp->next = list1;
+        }else{
+            temp->next = list2;
+        }
+
+        return dummy->next;
+    }
+
     ListNode* sortList(ListNode* head) {
-        vector<int> ans;
-        ListNode* temp = head;
+        if(!head || !head->next) return head;
 
-        while (temp) {
-            ans.push_back(temp->val);
-            temp = temp->next;
+        ListNode* slow = head;
+        ListNode* fast = head->next;
+
+        while(fast && fast->next){
+            slow = slow->next;
+            fast = fast->next->next;
         }
 
-        sort(ans.begin(), ans.end());
+        ListNode* left = head;
+        ListNode* right = slow->next;
 
-        temp = head;
+        slow->next = nullptr;
 
-        int i = 0;
-        while (temp) {
-            temp->val = ans[i];
-            temp = temp->next;
-            i++;
-        }
+        left = sortList(left);
+        right = sortList(right);
 
-        return head;
+        return merge(left, right);
     }
 };
