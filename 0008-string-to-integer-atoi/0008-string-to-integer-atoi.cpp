375b0a0ab35f66c22e625d5a58af1f63dcd8d2d6
Ome Tiwari <omt81144@gmail.com>
2026-05-25 00:00:09 +0530

    Time: 0 ms (100%), Space: 10.4 MB (5.19%) - LeetHub

--- 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.cpp
@@ -1,36 +1,34 @@
 class Solution {
 public:
+
+    int rounding(int ans, int i, int n, string s, int sign){
+        if(i >= n || !isdigit(s[i])) return ans*sign;
+        
+        int digit = s[i] - '0';
+
+        if(ans > (INT_MAX - digit)/10){
+            return sign == 1? INT_MAX : INT_MIN;
+        }
+        ans = ans*10 + digit;
+
+        return rounding(ans, i+1, n, s, sign);
+    }
+
     int myAtoi(string s) {
-        int i = 0, n = s.length();
-
         int ans = 0;
+        int i =0, n = s.length();
 
-        // remove leading spacess
-        while (i < n && s[i] == ' ')
-            i++;
-
-
-        // check sign
+        // remove spaces
+        while(i<n && s[i]==' ') i++;
+    
+        // find sign
         int sign = 1;
-        if(i < n && (s[i] == '+' || s[i] == '-')) {
-            if(s[i] == '-') sign = -1;
+        if(i<n && (s[i]=='+' || s[i]=='-')){
+            if(s[i]=='-') sign = -1;
             i++;
         }
 
-
-        // ignore 0 and non digit
-        while(i<n && isdigit(s[i])){
-            int digit = s[i] - '0';
-
-            //overflow handle
-            if(ans > (INT_MAX - digit)/10){
-                return sign ==1 ? INT_MAX : INT_MIN; 
-            }
-
-            ans = ans*10 + digit;   
-            i++;
-        }
-
-        return sign*ans;
+        //recuresive fxn with return;
+        return rounding(ans, i, n, s, sign);
     }
 };
