1a13e7c63b3eef37ba9f253cca4e63298a3ab3e0
Ome Tiwari <omt81144@gmail.com>
2026-05-26 19:25:36 +0530

    Time: 15 ms (18.02%), Space: 18.6 MB (18.42%) - LeetHub

---
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void getAllCombination(vector<int>& nums, int target, set<vector<int>>&ans, vector<int>& curr, int idx){
+
+        if(idx == nums.size()) return;
+
+        if(target < 0){
+            return;
+        }
+
+        if(target == 0){
+            ans.insert(curr);
+        }
+
+
+        //include this and move to next
+        curr.push_back(nums[idx]);
+        getAllCombination(nums, target-nums[idx], ans, curr, idx+1);
+
+        // include this multiple time
+        getAllCombination(nums, target-nums[idx], ans, curr, idx);
+
+        //backtrack
+        curr.pop_back();
+
+        // exclude this and move to next
+        getAllCombination(nums, target, ans, curr, idx+1);
+
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        set<vector<int>> ans;
+        vector<int> curr;
+
+        getAllCombination(candidates, target, ans, curr, 0);
+        return vector<vector<int>>(ans.begin(), ans.end());
+    }
+};
