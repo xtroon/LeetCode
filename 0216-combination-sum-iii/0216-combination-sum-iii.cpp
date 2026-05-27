a997a7ab935bac3fee47550bbeb9bcbda2dfb287
Ome Tiwari <omt81144@gmail.com>
2026-05-27 11:13:55 +0530

    Time: 0 ms (100%), Space: 8.7 MB (79.69%) - LeetHub

--- 0216-combination-sum-iii/0216-combination-sum-iii.cpp
+++ 0216-combination-sum-iii/0216-combination-sum-iii.cpp
@@ -1,22 +1,30 @@
 class Solution {
 public:
     void getComb(vector<int>& nums, int idx, int k, int n, vector<int>& curr, vector<vector<int>>& ans){
-
+        //base cases
         if(curr.size() == k && n==0){
             ans.push_back(curr);
             return;
         }
 
-        if(idx >= nums.size()) return;
-        
+        //optimised base case
+        if(idx >= nums.size() || n<0 || curr.size() > k) return;
+
+        // take it and move next
         curr.push_back(nums[idx]);
         getComb(nums, idx+1, k, n-nums[idx], curr, ans);
 
+        //backtrack
         curr.pop_back();
+
+        //don't take it and move next
         getComb(nums, idx+1, k, n, curr, ans);
     }
+
     vector<vector<int>> combinationSum3(int k, int n) {
+        //initialize nums
         vector<int> nums = {1,2,3,4,5,6,7,8,9};
+        
         vector<vector<int>> ans;
         vector<int> curr;
 
