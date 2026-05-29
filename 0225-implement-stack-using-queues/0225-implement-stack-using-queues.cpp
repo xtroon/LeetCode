0c0731bb3b9cd88cc9e603e76125b052c6a0e027
Ome Tiwari <omt81144@gmail.com>
2026-05-29 14:35:13 +0530

    Time: 0 ms (100%), Space: 9.6 MB (46.66%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -3,39 +3,26 @@ public:
     queue<int> q1;
     queue<int> q2;
 
-    MyStack() {
-        
-    }
-    
-    void push(int x) {
-        if(q1.empty()) q1.push(x);
-        else{
-            while(!q1.empty()){
-                q2.push(q1.front());
-                q1.pop();
-            }
-            q1.push(x);
-            while(!q2.empty()){
-                q1.push(q2.front());
-                q2.pop();
-            }
-        }
+    MyStack() {}
 
+    void push(int x) {
+        q2.push(x);
+        while(!q1.empty()){
+            q2.push(q1.front());
+            q1.pop();
+        }
+        swap(q1,q2);
     }
-    
+
     int pop() {
         int ans = q1.front();
         q1.pop();
         return ans;
     }
-    
-    int top() {
-        return q1.front();
-    }
-    
-    bool empty() {
-        return q1.empty();
-    }
+
+    int top() { return q1.front(); }
+
+    bool empty() { return q1.empty(); }
 };
 
 /**
